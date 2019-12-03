6d3f93cf513dc2e61f3095307d50238ce970fe26
Sergio RUIZ GARCIA <sergio.ruizgarcia@kleegroup.com>
2019-12-03 10:00:07 +0100

    The break statement only got out of the innermost loop; we need to stop the execution instead

--- 2019/src/main/java/day2/Solution02.java
+++ 2019/src/main/java/day2/Solution02.java
@@ -11,8 +11,6 @@ public class Solution02 {
 	
 	public static void main(String[] args) throws IOException {
 		List<String> inputLines = FileUtils.readFromFile("src/main/resources/day2/input_02.txt");
-		int noun = 0;
-		int verb = 0;
 		
 		int[] initialMemoryState = Arrays.asList(inputLines.get(0).split(",")).stream().mapToInt(Integer::valueOf).toArray();
 		int targetValue = 19690720;
@@ -20,13 +18,12 @@ public class Solution02 {
 		for (int i = 0; i < 100; i++) {
 			for (int j = 0; j < 100; j++) {
 				if (IntcodeComputer.executeProgram(initialMemoryState, i, j) == targetValue) {
-					noun = i;
-					verb = j;
-					break;
+					FileUtils.writeToFile("src/output/day2/output_02.txt", String.valueOf(i * 100 + j));
+					return;
 				}
 			}
 		}
 		
-		FileUtils.writeToFile("src/output/day2/output_02.txt", String.valueOf(noun * 100 + verb));
+		
 	}
 }
